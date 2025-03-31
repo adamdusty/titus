3a209e3f29ac1ac638985a02d19f2486e063eec7
Adam Dusty <adam.dusty27@gmail.com>
2025-03-30 18:41:47 -0700

    Changed example name from testing to exploratory

--- examples/exploratory/src/exploratory.c
+++ examples/exploratory/src/exploratory.c
@@ -1,15 +1,13 @@
 #include <titus/sdk.h>
 
-#include "testing/export.h"
+#include "exploratory/export.h"
 
 #include <core/core.h>
 #include <flecs.h>
-#include <math.h>
 
-void testing_system(ecs_iter_t* it);
-static float t = 0;
+EXPLORATORY_EXPORT void titus_initialize(const TitusApplicationContext* ctx, sds module_root_directory) {}
 
-TG_EXPORT void titus_initialize(const TitusApplicationContext* ctx, sds module_root_directory) {
+/*
     titus_log_info("Hello from testing");
 
     ECS_SYSTEM(ctx->ecs, testing_system, EcsOnUpdate, core.CoreCamera, core.CorePosition);
@@ -22,14 +20,5 @@ TG_EXPORT void titus_initialize(const TitusApplicationContext* ctx, sds module_r
     CoreMesh* cube_mesh = ecs_ensure(ctx->ecs, cube, CoreMesh);
     *cube_mesh          = core_create_cube_mesh(0.5f, 0.5f, 0.5f);
     ecs_modified(ctx->ecs, cube, CoreMesh);
-}
 
-void testing_system(ecs_iter_t* it) {
-    CorePosition* pos = ecs_field(it, CorePosition, 1);
-    t += it->delta_time;
-
-    for(int i = 0; i < it->count; i++) {
-        pos[i][0] = sinf(t * 1.0f) * 5.0f;
-        pos[i][1] = sinf(t * 1.0f) * 5.0f;
-    }
-}
+*/
