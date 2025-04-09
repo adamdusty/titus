6705413eaaf50e29da5661a39f996427b0e12195
Adam Dusty <adam.dusty27@gmail.com>
2025-04-08 20:03:15 -0700

    New input components

--- core/core/include/core/input.h
+++ core/core/include/core/input.h
@@ -6,3 +6,22 @@ typedef struct CoreFrameInput {
     size_t count;
     SDL_Event events[255];
 } CoreFrameInput;
+
+typedef enum CORE_KEY_STATE {
+    CORE_KEY_STATE_DOWN,
+    CORE_KEY_STATE_UP,
+} CORE_KEY_STATE;
+
+typedef enum CORE_BUTTON_STATE {
+    CORE_BUTTON_STATE_DOWN,
+    CORE_BUTTON_STATE_UP,
+} CORE_BUTTON_STATE;
+
+typedef struct CoreFrameInputState {
+    CORE_KEY_STATE* scancodes;        // input.scancode[SDL_SCANCODE_A]
+    CORE_BUTTON_STATE* mouse_buttons; // input.mouse_buttons[0]
+} CoreFrameInputState;
+
+typedef struct CoreFrameInputComponent {
+    CoreFrameInputState state[2]; // state[0] current, state[1] previous
+} CoreFrameInputComponent;
